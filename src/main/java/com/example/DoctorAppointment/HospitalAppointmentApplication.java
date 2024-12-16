f273a7bd258f6249b509f28e7bc7ca2b6101e7d9
sampathmettu <sampathmettu@gmail.com>
2024-12-16 12:54:30 +0530

    Update HospitalAppointmentApplication.java
    http://bucks32024.s3-website.ap-south-1.amazonaws.com

--- src/main/java/com/example/DoctorAppointment/HospitalAppointmentApplication.java
+++ src/main/java/com/example/DoctorAppointment/HospitalAppointmentApplication.java
@@ -19,7 +19,7 @@ public class HospitalAppointmentApplication {
 
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-				.allowedOrigins("https://http://bucks32024.s3-website.ap-south-1.amazonaws.com")
+				.allowedOrigins("https://bucks32024.s3-website.ap-south-1.amazonaws.com")
 				.allowedMethods("GET", "POST", "PUT", "DELETE")
 				.allowedHeaders("*");
 
